46d48f7fffaad42e21526e59f18ccd58bf1496cd
marinm <mestrovic.ma@gmail.com>
2016-11-10 02:50:57 -0500

    test performance with media=print tag

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('scripts', function(){
 });
 
 gulp.task('minify-css', function() {
-    return gulp.src('css/*.css')
+    return gulp.src('css/style.css')
       .pipe(cleanCSS({debug: true}, function(details) {
           console.log(details.name + ': ' + details.stats.originalSize);
           console.log(details.name + ': ' + details.stats.minifiedSize);
